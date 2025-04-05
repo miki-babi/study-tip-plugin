5e6e2fe19d6828d6ba2a447bcec214e3c0fc7439
miki-babi <mikiyasshiferaw@gmail.com>
2025-04-05 12:33:50 +0300

    test

--- index.php
+++ index.php
@@ -33,4 +33,27 @@ add_shortcode('study_tip', 'study_tip_shortcode');
 function study_tip_enqueue_styles() {
     wp_enqueue_style('study-tip-style', plugin_dir_url(__FILE__) . 'assets/style.css');
 }
+//submenu 
+function wporg_options_page_html() {
+	// check user capabilities
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+	?>
+	<div class="wrap">
+		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+		<form action="options.php" method="post">
+			<?php
+			// output security fields for the registered setting "wporg_options"
+			settings_fields( 'wporg_options' );
+			// output setting sections and their fields
+			// (sections are registered for "wporg", each field is registered to a specific section)
+			do_settings_sections( 'wporg' );
+			// output save settings button
+			submit_button( __( 'Save Settings', 'textdomain' ) );
+			?>
+		</form>
+	</div>
+	<?php
+}
 add_action('wp_enqueue_scripts', 'study_tip_enqueue_styles');
